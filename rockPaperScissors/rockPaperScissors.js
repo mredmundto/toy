ada9f4108e1b3485409cac29691917f129c4d12d
Fred Zirdung <fred@hackreactor.com>
2016-04-27 09:30:14 -0700

    Solution: rockPaperScissors

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,28 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function (rounds) {
   // TODO: your solution here
-};
+
+  var plays = ['rock', 'paper', 'scissors'];
+  var rounds = rounds || 3;
+  var combos = [];
+
+  var generateCombos = function (roundsToGo, playedSoFar) {
+    playedSoFar = playedSoFar || [];
+    
+    if (roundsToGo === 0) {
+      combos.push(playedSoFar);
+      return;
+    }
+    
+    for (var i = 0; i < plays.length; i++) {
+      var currentPlay = plays[i];
+      generateCombos(roundsToGo - 1, playedSoFar.concat(currentPlay));
+    }
+    
+  }
+
+  generateCombos(rounds);
+  return combos;
+  };
